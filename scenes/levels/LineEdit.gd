a0893f76f10623bbd059a47e5cab098227cbc922
David Itkin <ditkin@hubspot.com>
2022-09-25 16:28:07 -0400

    Add teleport and swirly exit picture

---
+++ scenes/levels/LineEdit.gd
@@ -0,0 +1,26 @@
+extends LineEdit
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var answer = "dreams lie in a slice of sweetness and warmth"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if self.text == answer:
+		$Swirly.show()
+		yield(get_tree().create_timer(1.0), "timeout")
+		Globals.spawnpoint = "house_outside"
+		get_tree().change_scene("res://scenes/levels/Outside.tscn")
+	pass
+	
+
